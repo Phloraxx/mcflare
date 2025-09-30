b2b271c769d0e0cc5603e39c4fd19193e060ab96
HttpRafa <60099368+HttpRafa@users.noreply.github.com>
2025-09-30 18:45:27 +0200

    feat: 1.21.9

--- common/src/main/java/de/rafael/modflared/mixin/client/ServerEntryMixin.java
+++ common/src/main/java/de/rafael/modflared/mixin/client/ServerEntryMixin.java
@@ -24,7 +24,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.Collections;
 
 @Mixin(MultiplayerServerListWidget.ServerEntry.class)
-public abstract class ServerEntryMixin {
+public abstract class ServerEntryMixin extends MultiplayerServerListWidget.Entry {
 
     @Shadow @Final private ServerInfo server;
 
@@ -32,17 +32,19 @@ public abstract class ServerEntryMixin {
     private static final Identifier MODFLARED_INDICATOR_TEXTURE = Identifier.of(Modflared.MOD_ID, "icon/indicator");
 
     @Inject(method = "render", at = @At("TAIL"))
-    public void render(@NotNull DrawContext context, int index, int y, int x, int entryWidth, int entryHeight, int mouseX, int mouseY, boolean hovered, float tickDelta, CallbackInfo ci) {
+    public void render(DrawContext context, int mouseX, int mouseY, boolean hovered, float deltaTicks, CallbackInfo ci) {
         var tunnelStatus = ((IServerInfo) server).getTunnelStatus();
         if(tunnelStatus != null && tunnelStatus.state() == TunnelStatus.State.USE) {
-            int xOffset = entryWidth - 15;
+            int xOffset = this.getContentWidth() - 15;
             int yOffset = 10 + 1;
+            int x = this.getContentX();
+            int y = this.getContentY();
             context.drawGuiTexture(RenderPipelines.GUI_TEXTURED, MODFLARED_INDICATOR_TEXTURE, x + xOffset, y + yOffset, 10, 10);
 
             // Tooltip
             int l = mouseX - x;
             int m = mouseY - y;
-            if (l >= entryWidth - 15 && l <= entryWidth - 5 && m >= 9 && m <= 9 + 10) {
+            if (l >= this.getContentWidth() - 15 && l <= this.getContentWidth() - 5 && m >= 9 && m <= 9 + 10) {
                 context.drawTooltip(Text.translatable("gui.multiplayer.tunnel.status.0").formatted(Formatting.AQUA), mouseX, mouseY);
             }
         }
